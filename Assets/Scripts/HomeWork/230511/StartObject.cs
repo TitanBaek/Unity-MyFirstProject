e9876e73407f5cfe7a7d312fdab1911d1c8cb512
인권 백 <baek626@gmail.com>
2023-05-11 21:49:03 +0900

    feat : 과제찐찐찐최종

--- Assets/Scripts/HomeWork/230511/StartObject.cs
+++ Assets/Scripts/HomeWork/230511/StartObject.cs
@@ -11,11 +11,16 @@ public class StartObject : MonoBehaviour
     [Header("Set JumpForce")]               // 인스펙터의 점프 강도 부분 네이밍
     private float jumpForce = 5f;           // 점프 강도
 
+    private void Awake()
+    {
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        playerObject.name = "ImPlayer";                         // 플레이어 오브젝트의 이름을 바꿔주는 부분 그렇기 때문에 Awake에 안쓰고 Start에 써주었다.
+        // 외부의 객체(playerObject와 playerObject의 Rigidbody를 건드리는 작업이기 때문에 
+        // Awake가 아닌 Start에서 처리해준다.
+        playerObject.name = "ImPlayer";                         // 플레이어 오브젝트의 이름을 바꿔주는 부분
         rb = playerObject.GetComponent<Rigidbody>();            // rb에 참조된 playerObject의 Rigidbody를 참조
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // playerObject의 rb에 AddForce를 주어 게임이 시작되면 playerObject는 jumpForce강도로 점프한다.
     }
